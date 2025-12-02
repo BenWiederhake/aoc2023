9cb1cd0edb90f261ca1ccd446e8bd0d599357b51
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2025-12-02 17:06:40 +0100

    Solve 2025 day 2, part 2: Any amount of repetitions

--- 2025day02/Program.cs
+++ 2025day02/Program.cs
@@ -1,17 +1,20 @@
-﻿using System.Collections.Generic;
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Text;
 
 class Solver
 {
-    public long firstHalf;
+    public long FirstHalf;
+    public int Repetitions;
 
-    public Solver(string composite) {
+    public Solver(string composite, int repetitions) {
         long compositeAsInt = Int64.Parse(composite);
-        if (composite.Length < 2) {
-            firstHalf = 1;
+        Repetitions = repetitions;
+        if (composite.Length < Repetitions) {
+            FirstHalf = 1;
         } else {
-            firstHalf = Int64.Parse(composite[0..(composite.Length / 2)]); // round down
+            FirstHalf = Int64.Parse(composite[0..(composite.Length / Repetitions)]); // round down
         }
         while (CurrentNumber() < compositeAsInt) {
             Increment();
@@ -19,11 +22,16 @@ class Solver
     }
 
     public long CurrentNumber() {
-        return Int64.Parse("" + firstHalf + firstHalf);
+        var sb = new StringBuilder();
+        var partAsString = FirstHalf.ToString();
+        for (int i = 0; i < Repetitions; ++i) {
+            sb.Append(partAsString);
+        }
+        return Int64.Parse(sb.ToString());
     }
 
     public void Increment() {
-        firstHalf += 1;
+        FirstHalf += 1;
     }
 
     static void Main(string[] args)
@@ -37,17 +45,23 @@ class Solver
             Debug.Assert(sr != null);
             string line = sr.ReadLine();
             Debug.Assert(line != null);
-            long sumOfInvalidIds = 0;
+            var invalidIds = new HashSet<long>();
             foreach (string interval in line.Split(',')) {
                 Console.WriteLine(interval);
                 string[] parts = interval.Split('-');
                 var end = Int64.Parse(parts[1]);
-                for (var enumerator = new Solver(parts[0]); enumerator.CurrentNumber() <= end; enumerator.Increment()) {
-                    Console.WriteLine(" -> " + enumerator.CurrentNumber());
-                    sumOfInvalidIds += enumerator.CurrentNumber();
+                for (int repetitions = 2; repetitions < 20; ++repetitions) {
+                    for (var enumerator = new Solver(parts[0], repetitions); enumerator.CurrentNumber() <= end; enumerator.Increment()) {
+                        Console.WriteLine(" -> " + enumerator.CurrentNumber());
+                        invalidIds.Add(enumerator.CurrentNumber());
+                    }
                 }
             }
-            Console.WriteLine(sumOfInvalidIds);
+            long sumOfInvalidIds = 0;
+            foreach (long invalidId in invalidIds) {
+                sumOfInvalidIds += invalidId;
+            }
+            Console.WriteLine("sum = " + sumOfInvalidIds);
         }
     }
 }
