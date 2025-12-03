dcb39c3dea0c29cefdd55a280f521c3ee128c366
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2025-12-03 09:45:49 +0100

    Solve 2025 day 3, part 1

---
+++ 2025day03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+class Solver
+{
+    static int FindJoltage(char[] bank) {
+        char firstChar = bank[..(bank.Length - 1)].Max();
+        int firstCharIndex = Array.IndexOf(bank, firstChar);
+        char secondChar = bank[(firstCharIndex + 1)..].Max();
+        int result = Int32.Parse("" + firstChar + secondChar);
+        Console.WriteLine("+ " + result);
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        string filename = "input";
+        if (args.Length > 0) {
+            filename = args[0];
+        }
+        int sumOfJoltages = 0;
+        using (StreamReader sr = new StreamReader(filename))
+        {
+            Debug.Assert(sr != null);
+            string line;
+            while ((line = sr.ReadLine()) != null) {
+                sumOfJoltages += FindJoltage(line.ToCharArray());
+            }
+        }
+        Console.WriteLine("sum = " + sumOfJoltages);
+    }
+}
