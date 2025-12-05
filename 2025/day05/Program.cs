3c8c1e8570903c243cdd73a2cd7543a31e952750
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2025-12-05 10:58:52 +0100

    Solve 2025 day 05, part 1

---
+++ 2025/day05/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+// `Range` instances are immutable.
+struct Range : IComparable<Range> {
+    public int Lo { get; }
+    public int Hi { get; }
+
+    public Range(int lo, int hi) {
+        Debug.Assert(lo <= hi);
+        Lo = lo;
+        Hi = hi;
+    }
+
+    public int CompareTo(Range that)
+    {
+        if (this.Lo <  that.Lo) return -1;
+        if (this.Lo == that.Lo) return 0;
+        return 1;
+    }
+}
+
+// `RangeSet` instances are immutable.
+class RangeSet {
+    private List<Range> SortedRanges;
+
+    public RangeSet(List<Range> sortedRanges) {
+        Console.WriteLine("Got " + sortedRanges.Count + " sorted (and merged) ranges:");
+        foreach (var e in sortedRanges) {
+            Console.WriteLine(" * " + e.Lo + "-" + e.Hi);
+        }
+        SortedRanges = sortedRanges;
+    }
+
+    // - Either returns a valid index of a range whose "Lo" is less-or-equal the requested ingredientId (and is the maximum such index),
+    // - or returns -1.
+    public int IndexOfFloorRange(int ingredientId) {
+        // https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.list-1.binarysearch?view=net-10.0
+        // """
+        //     The zero-based index of item in the sorted List<T>, if item is found; otherwise, a negative number
+        //     that is the bitwise complement of the index of the next element that is larger than item or,
+        //     if there is no larger element, the bitwise complement of Count.
+        // """
+        // I mean, yes, okay, it *does* conver all cases, but was there really no better way to represent this information?
+        // No enum or option type? Really? Oof.
+        int binarySearchIndex = SortedRanges.BinarySearch(new Range(ingredientId, ingredientId));
+        if (binarySearchIndex >= 0) {
+            // Exact match found! "Floor" is just identity function in this case:
+            return binarySearchIndex;
+        }
+        // No exact match found! First, determine a "normal" (i.e. non-negative) index:
+        int ceilingIndex = ~binarySearchIndex;
+        // `ceilingIndex` might point to the range *after* the last range.
+        // Just to make sure I understood this conversion and bitfiddling correctly:
+        Debug.Assert(0 <= ceilingIndex && ceilingIndex <= SortedRanges.Count);
+        return ceilingIndex - 1;
+    }
+
+    public bool Contains(int ingredientId) {
+        int onlyRelevantIndex = IndexOfFloorRange(ingredientId);
+        // Console.WriteLine($"Looking up {ingredientId}, will look only at index {onlyRelevantIndex}");
+        if (onlyRelevantIndex < 0) {
+            return false;
+        }
+        Range r = SortedRanges[onlyRelevantIndex];
+        // Console.WriteLine($"  ... which is {r.Lo}-{r.Hi}");
+        // if (onlyRelevantIndex + 1 < SortedRanges.Count) {
+        //     var next = SortedRanges[onlyRelevantIndex + 1];
+        //     Console.WriteLine($"  ... followed by {next.Lo}-{next.Hi}");
+        // }
+        // Just to sanity check: This is the last range whose `Lo` is less-or-equal the requested ingredientId, right?
+        Debug.Assert(r.Lo <= ingredientId);
+        Debug.Assert(onlyRelevantIndex + 1 == SortedRanges.Count || ingredientId < SortedRanges[onlyRelevantIndex + 1].Lo);
+        return ingredientId <= r.Hi;
+    }
+}
+
+// `Solver` instances are mutable, similar to `StringBuilder`.
+class Solver
+{
+    private List<Range> UnsortedRanges = new();
+
+    public Solver() {
+    }
+
+    public void AddFreshRange(string lo, string hi) {
+        UnsortedRanges.Add(new Range(Int32.Parse(lo), Int32.Parse(hi)));
+    }
+
+    public RangeSet ToRangeSet() {
+        Console.WriteLine("Got " + UnsortedRanges.Count + " raw (unsorted, unmerged) ranges:");
+        foreach (var e in UnsortedRanges) {
+            Console.WriteLine(" * " + e.Lo + "-" + e.Hi);
+        }
+        UnsortedRanges.Sort();
+        List<Range> sortedRanges = new();
+        Range? pending = null;
+        foreach (var range in UnsortedRanges) {
+            if (pending.HasValue && pending.Value.Hi + 1 < range.Lo) {
+                // Flush:
+                sortedRanges.Add(pending.Value);
+                pending = null;
+                // Fall-through to "Take"
+            }
+            if (!pending.HasValue) {
+                // Take:
+                pending = range;
+            } else {
+                // Merge:
+                pending = new Range(pending.Value.Lo, range.Hi);
+            }
+        }
+        if (pending.HasValue) {
+            // Flush:
+            sortedRanges.Add(pending.Value);
+            pending = null;
+        }
+        return new RangeSet(sortedRanges);
+    }
+
+    static void Main(string[] args)
+    {
+        string filename = "input";
+        if (args.Length > 0) {
+            filename = args[0];
+        }
+        int numFreshIngredients = 0;
+        using (StreamReader sr = new StreamReader(filename))
+        {
+            string line;
+            Solver solver = new();
+            while ((line = sr.ReadLine()) != null && (line != "")) {
+                var parts = line.Split('-');
+                solver.AddFreshRange(parts[0], parts[1]);
+            }
+            var rangeSet = solver.ToRangeSet();
+            while ((line = sr.ReadLine()) != null) {
+                if (rangeSet.Contains(Int32.Parse(line))) {
+                    numFreshIngredients += 1;
+                }
+            }
+        }
+        Console.WriteLine("numFresh = " + numFreshIngredients);
+    }
+}
